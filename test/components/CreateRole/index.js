803287df6ecae226931d464ec52c901949e2b539
Alexander Lachmann <32603869+lachmann12@users.noreply.github.com>
2022-06-27 13:12:58 -0400

    update

--- test/components/CreateRole/index.js
+++ test/components/CreateRole/index.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import styles from "./createrole.module.css";
+import {Container, Row, Col} from 'react-bootstrap'
+
 
 const CreateRole = () => {
   return (
--- test/components/Footer/index.js
+++ test/components/Footer/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Image } from 'next/image';
 import styles from './footer.module.css'
 
 const Footer = ({ children, ...rest }) => {
