f3a2543fb481dec163e79cd2c5a23813b6d2098b
Alexander Lachmann <32603869+lachmann12@users.noreply.github.com>
2022-07-26 15:45:47 -0400

    Update index.js

--- pages/rest/index.js
+++ pages/rest/index.js
@@ -2,6 +2,7 @@
 import React from "react";
 import dynamic from 'next/dynamic'
 import "swagger-ui-react/swagger-ui.css";
+import { Config } from '../../config/Config.js'; 
 
 const SwaggerUI = dynamic(() => import('swagger-ui-react'), { ssr: false })
 
